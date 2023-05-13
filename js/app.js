1dcbe4fd1ae6f667db781b288c1f5b3610ddd5a9
zoro <mohndlolo3@gmail.com>
2023-05-13 20:37:15 +0200

    complating the project

--- js/app.js
+++ js/app.js
@@ -17,7 +17,12 @@ menu.addEventListener("click", () => {
 // creating images for the main img
 let main_pic_par = document.querySelector(".main-pic");
 let toggle = document.querySelector(".toggles");
-let main_img = ["black-clover.jpg", "demon-slayer.jpg", "one-piece.jpg"],
+let main_img = [
+    "black-clover.jpg",
+    "tanjero(fi).jpg",
+    "one-piece.jpg",
+    "fate-zero.jpg",
+  ],
   folder = "main-pic/";
 
 for (i = 0; i < main_img.length; i++) {
@@ -52,3 +57,46 @@ function removeClasses(el) {
     e.classList.remove("active");
   });
 }
+
+// scrolling
+
+let rated = document.querySelector(".top-rated");
+let update = document.querySelector(".last-update");
+let popular = document.querySelector(".most-popular");
+let coming = document.querySelector(".coming-soon");
+let btn_rated = document.getElementById("top-rated");
+let btn_update = document.getElementById("last-update");
+let btn_popular = document.getElementById("most-popular");
+let btn_coming = document.getElementById("coming-soon");
+let home = document.getElementById("home");
+
+console.log(rated.offsetTop);
+console.log(update.offsetTop);
+console.log(popular.offsetTop);
+console.log(coming.offsetTop);
+
+btn_rated.onclick = () => {
+  window.scrollTo({
+    top: rated.offsetTop,
+  });
+};
+btn_update.onclick = () => {
+  window.scrollTo({
+    top: update.offsetTop,
+  });
+};
+btn_popular.onclick = () => {
+  window.scrollTo({
+    top: popular.offsetTop,
+  });
+};
+btn_coming.onclick = () => {
+  window.scrollTo({
+    top: coming.offsetTop,
+  });
+};
+home.onclick = () => {
+  window.scrollTo({
+    top: 0,
+  });
+};
